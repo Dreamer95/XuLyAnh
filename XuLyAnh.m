dd806766be6e83ead796eb4ead01e1e9646c5f53
Dreamer95 <ngocdong561995@gmail.com>
2017-02-21 14:09:59 +0700

    Final

--- XuLyAnh.m
+++ XuLyAnh.m
@@ -22,7 +22,7 @@ function varargout = XuLyAnh(varargin)
 
 % Edit the above text to modify the response to help XuLyAnh
 
-% Last Modified by GUIDE v2.5 19-Feb-2017 22:52:10
+% Last Modified by GUIDE v2.5 21-Feb-2017 00:59:47
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -86,9 +86,13 @@ global Img
 global pic
 set(handles.radiobutton2,'Value',0)
 set(handles.radiobutton3,'Value',0)
-%set(handles.radiobutton4,'Value',0)
-%set(handles.radiobutton5,'Value',0)
-%set(handles.slider1,'Value',0)
+set(handles.radioGrayScale,'Value',0)
+set(handles.radioAmBan,'Value',0)
+set(handles.slider1,'Value',0)
+set(handles.slider2,'Value',0)
+set(handles.slider3,'Value',0)
+set(handles.slider4,'Value',0)
+set(handles.slider5,'Value',0)
 %set(handles.edit1,'String',0)
 
 
@@ -100,6 +104,7 @@ axes(handles.axes2)
 cla('reset')
 imshow(pic)
 title('Red Channel')
+set(handles.textBinary,'String','')
 
 
 % Hint: get(hObject,'Value') returns toggle state of radiobutton1
@@ -114,9 +119,13 @@ global Img
 global pic
 set(handles.radiobutton1,'Value',0)
 set(handles.radiobutton3,'Value',0)
-%set(handles.radiobutton4,'Value',0)
-%set(handles.radiobutton5,'Value',0)
-%set(handles.slider1,'Value',0)
+set(handles.radioGrayScale,'Value',0)
+set(handles.radioAmBan,'Value',0)
+set(handles.slider1,'Value',0)
+set(handles.slider2,'Value',0)
+set(handles.slider3,'Value',0)
+set(handles.slider4,'Value',0)
+set(handles.slider5,'Value',0)
 %set(handles.edit1,'String',0)
 
 % Img = handles.Img;
@@ -128,6 +137,7 @@ axes(handles.axes2)
 % cla('reset')
 imshow(pic)
 title('Blue Channel')
+set(handles.textBinary,'String','')
 
 % Hint: get(hObject,'Value') returns toggle state of radiobutton2
 
@@ -141,9 +151,14 @@ global Img
 global pic
 set(handles.radiobutton1,'Value',0)
 set(handles.radiobutton2,'Value',0)
-%set(handles.radiobutton4,'Value',0)
-%set(handles.radiobutton5,'Value',0)
-%set(handles.slider1,'Value',0)
+set(handles.radioGrayScale,'Value',0)
+set(handles.radioAmBan,'Value',0)
+set(handles.slider1,'Value',0)
+set(handles.slider1,'Value',0)
+set(handles.slider2,'Value',0)
+set(handles.slider3,'Value',0)
+set(handles.slider4,'Value',0)
+set(handles.slider5,'Value',0)
 %set(handles.edit1,'String',0)
 
 % Img = handles.Img;
@@ -155,6 +170,7 @@ axes(handles.axes2)
 cla('reset')
 imshow(pic)
 title('Green Channel')
+set(handles.textBinary,'String','')
 
 % Hint: get(hObject,'Value') returns toggle state of radiobutton3
 
@@ -187,10 +203,276 @@ guidata(hObject,handles);
 set(handles.radiobutton1,'Enable','on')
 set(handles.radiobutton2,'Enable','on')
 set(handles.radiobutton3,'Enable','on')
-%set(handles.radiobutton4,'Enable','on')
-%set(handles.radiobutton5,'Enable','on')
-%set(handles.slider1,'Enable','on')
+set(handles.radioGrayScale,'Enable','on')
+set(handles.radioAmBan,'Enable','on')
+set(handles.slider1,'Enable','on')
+set(handles.slider2,'Enable','on')
+set(handles.slider3,'Enable','on')
+set(handles.slider4,'Enable','on')
 %set(handles.edit1,'Enable','on')
+
+
+
+
+% --- Executes on slider movement.
+function slider2_Callback(hObject, eventdata, handles)
+% hObject    handle to slider2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+global Img pic
+set(handles.radiobutton2,'Value',0)
+set(handles.radiobutton3,'Value',0)
+set(handles.radiobutton1,'Value',0)
+set(handles.radioGrayScale,'Value',0)
+set(handles.radioAmBan,'Value',0)
+set(handles.slider1,'Value',0)
+set(handles.slider5,'Value',0)
+set(handles.slider3,'Value',0)
+set(handles.slider4,'Value',0)
+set(handles.slider5,'Value',0)
+E = get(hObject,'Value');
+g = im2double(Img);
+m = mean2(g);%tính trung binh ma tran anh
+pic = 1./(1+(m./(g + eps)).^E);
+axes(handles.axes2);
+imshow(pic);
+title('Contrast Image');
+set(handles.textBinary,'String','')
+
+
+% Hints: get(hObject,'Value') returns position of slider
+%        get(hObject,'Min') and get(hObject,'Max') to determine range of slider
+
+
+% --- Executes during object creation, after setting all properties.
+function slider2_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to slider2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: slider controls usually have a light gray background.
+if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor',[.9 .9 .9]);
+end
+
+
+% --- Executes on slider movement.
+function slider3_Callback(hObject, eventdata, handles)
+% hObject    handle to slider3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+global Img pic
+set(handles.radiobutton2,'Value',0)
+set(handles.radiobutton3,'Value',0)
+set(handles.radiobutton1,'Value',0)
+set(handles.radioGrayScale,'Value',0)
+set(handles.radioAmBan,'Value',0)
+set(handles.slider1,'Value',0)
+set(handles.slider2,'Value',0)
+set(handles.slider4,'Value',0)
+set(handles.slider5,'Value',0)
+c = get(hObject,'Value');
+g = im2double(Img);
+pic = c*log(1 + double(g));
+axes(handles.axes2);
+imshow(pic);
+title('Contrast Image');
+set(handles.textBinary,'String','')
+
+
+
+% Hints: get(hObject,'Value') returns position of slider
+%        get(hObject,'Min') and get(hObject,'Max') to determine range of slider
+
+
+% --- Executes during object creation, after setting all properties.
+function slider3_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to slider3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: slider controls usually have a light gray background.
+if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor',[.9 .9 .9]);
+end
+
+
+% --- Executes on slider movement.
+function slider4_Callback(hObject, eventdata, handles)
+% hObject    handle to slider4 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+global Img pic
+set(handles.radiobutton2,'Value',0)
+set(handles.radiobutton3,'Value',0)
+set(handles.radiobutton1,'Value',0)
+set(handles.radioGrayScale,'Value',0)
+set(handles.radioAmBan,'Value',0)
+set(handles.slider1,'Value',0)
+set(handles.slider2,'Value',0)
+set(handles.slider3,'Value',0)
+%set(handles.slider5,'Value',0)
+c = get(handles.slider5,'Value');
+gamma = get(hObject,'Value');
+g = im2double(Img);
+pic = c*(g.^gamma);
+axes(handles.axes2);
+imshow(pic);
+title('Contrast Image');
+set(handles.textBinary,'String','')
+
+% Hints: get(hObject,'Value') returns position of slider
+%        get(hObject,'Min') and get(hObject,'Max') to determine range of slider
+
+
+% --- Executes during object creation, after setting all properties.
+function slider4_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to slider4 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: slider controls usually have a light gray background.
+if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor',[.9 .9 .9]);
+end
+
+
+% --- Executes on button press in radioAmBan.
+function radioAmBan_Callback(hObject, eventdata, handles)
+% hObject    handle to radioAmBan (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+global Img
+global pic
+set(handles.radiobutton2,'Value',0)
+set(handles.radiobutton3,'Value',0)
+set(handles.radiobutton1,'Value',0)
+set(handles.radioGrayScale,'Value',0)
+set(handles.slider1,'Value',0)
+set(handles.slider2,'Value',0)
+set(handles.slider3,'Value',0)
+set(handles.slider4,'Value',0)
+set(handles.slider5,'Value',0)
+pic = imcomplement(Img);
+axes(handles.axes2);
+imshow(pic);
+title('Negative Image');
+set(handles.textBinary,'String','')
+
+% Hint: get(hObject,'Value') returns toggle state of radioAmBan
+
+
+% --- Executes on slider movement.
+function slider1_Callback(hObject, eventdata, handles)
+% hObject    handle to slider1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+global Img
+global pic
+set(handles.radiobutton2,'Value',0)
+set(handles.radiobutton3,'Value',0)
+set(handles.radiobutton1,'Value',0)
+set(handles.radioGrayScale,'Value',0)
+set(handles.radioAmBan,'Value',0)
+set(handles.slider2,'Value',0)
+set(handles.slider3,'Value',0)
+set(handles.slider4,'Value',0)
+set(handles.slider5,'Value',0)
+Gray = rgb2gray(Img);
+thresh = get(hObject,'Value');
+pic = im2bw(Gray,thresh);
+axes(handles.axes2)
+cla('reset')
+imshow(pic)
+title('Binary Image')
+set(handles.textBinary,'String',num2str(thresh))
+
+
+% Hints: get(hObject,'Value') returns position of slider
+%        get(hObject,'Min') and get(hObject,'Max') to determine range of slider
+
+
+% --- Executes during object creation, after setting all properties.
+function slider1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to slider1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: slider controls usually have a light gray background.
+if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor',[.9 .9 .9]);
+end
+
+
+% --- Executes on button press in radioGrayScale.
+function radioGrayScale_Callback(hObject, eventdata, handles)
+
+% hObject    handle to radioGrayScale (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+global Img
+global pic
+set(handles.radiobutton2,'Value',0)
+set(handles.radiobutton3,'Value',0)
+set(handles.radiobutton1,'Value',0)
+set(handles.radioAmBan,'Value',0)
+set(handles.slider1,'Value',0)
+set(handles.slider2,'Value',0)
+set(handles.slider3,'Value',0)
+set(handles.slider4,'Value',0)
+set(handles.slider5,'Value',0)
+pic = rgb2gray(Img);
+axes(handles.axes2)
+% cla('reset')
+imshow(pic)
+title('Grayscale Image')
+set(handles.textBinary,'String','')
+
+
+
+% Hint: get(hObject,'Value') returns toggle state of radioGrayScale
+
+
+
+
+% --- Executes on slider movement.
+function slider5_Callback(hObject, eventdata, handles)
+% hObject    handle to slider5 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+global Img pic
+set(handles.radiobutton2,'Value',0)
+set(handles.radiobutton3,'Value',0)
+set(handles.radiobutton1,'Value',0)
+set(handles.radioGrayScale,'Value',0)
+set(handles.radioAmBan,'Value',0)
+set(handles.slider1,'Value',0)
+set(handles.slider2,'Value',0)
+set(handles.slider3,'Value',0)
+%set(handles.slider5,'Value',0)
+gamma = get(handles.slider4,'Value');
+c = get(hObject,'Value');
+g = im2double(Img);
+pic = c*(g.^gamma);
+axes(handles.axes2);
+imshow(pic);
+title('Contrast Image');
+set(handles.textBinary,'String','')
+
+% Hints: get(hObject,'Value') returns position of slider
+%        get(hObject,'Min') and get(hObject,'Max') to determine range of slider
+
+
+% --- Executes during object creation, after setting all properties.
+function slider5_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to slider5 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: slider controls usually have a light gray background.
+if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor',[.9 .9 .9]);
+end
 
 function pushbutton2_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton2 (see GCBO)
@@ -202,3 +484,49 @@ global pic
            '*.tif','TIFF Files(*.tif)';... 
            '*.*','all image file'},'Luu anh da chon!','anhkq/'); 
   imwrite(pic,[pathname,filename]);  
+
+
+% --------------------------------------------------------------------
+function file_Callback(hObject, eventdata, handles)
+% hObject    handle to file (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --------------------------------------------------------------------
+function help_Callback(hObject, eventdata, handles)
+% hObject    handle to help (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --------------------------------------------------------------------
+function about_Callback(hObject, eventdata, handles)
+% hObject    handle to about (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+msgbox('Chuong trinh xu ly anh voi mot so chuc nang don gian tren Matlab','About');
+
+
+
+% --------------------------------------------------------------------
+function introduce_Callback(hObject, eventdata, handles)
+% hObject    handle to introduce (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+msgbox({'    Sinh Vien Thuc Hien' 'Dinh Ngoc Dong - 13520209' 'Tran Tien Dung - 13520129'},'Introduce');
+
+
+% --------------------------------------------------------------------
+function quit_Callback(hObject, eventdata, handles)
+% hObject    handle to quit (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+choice = questdlg('Would you like to quit?',...
+    'Choice menu', ...
+    'Yes','No','Yes');
+switch choice
+    case 'Yes'
+        close
+    case 'No'
+end
